d231fd7b51c7b1850ba1572eb49b72996c448356
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2024-09-17 10:43:23 +0300

    Update test_adapters.py

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -1,10 +1,16 @@
 """Adapters tests."""
 
+
 from __future__ import annotations
+
+import os
+import sys
+
+ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
 from simpml.tabular.adapters_pool import ManipulateAdapter
 
-
 def test_manipulate_adapter() -> None:
     """Test the `ManipulateAdapter` class."""
-    assert hasattr(ManipulateAdapter, "manipulate")

5e2b5b5ac0e64765f4827773585996ba39bd65ca
Miriam2040 <66244918+Miriam2040@users.noreply.github.com>
2024-09-17 10:43:52 +0300

    Update test_base.py

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,6 +3,13 @@
 from __future__ import annotations
 import pytest
 
+import os
+import sys
+
+ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
+
 from simpml.core.base import (
     Dataset,
     DataType,
